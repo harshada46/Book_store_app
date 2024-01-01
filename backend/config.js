32bebfab37a653ff96b660e832732143a9dd1b57
Harshada Trimukhe <harshada.trimukhe20@pccoepune.org>
2024-01-01 15:29:36 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 8999;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@cluster0.bxloyxh.mongodb.net/book_collection?retryWrites=true&w=majority";
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ShowBooks from './pages/ShowBooks';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBooks/>} />
+      <Route exact path='/books/details/:id' element={<ShowBooks/>} />
+      <Route path='/books/edit/:id' element={<EditBook/>} />
+      <Route path='/books/delete/:id' element={<DeleteBook/>} />
+    </Routes>
+  );
+};
+
+export default App
